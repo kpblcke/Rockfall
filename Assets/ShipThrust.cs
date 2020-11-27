8bfdc66bfccf0f67c9d980441f64c47f245a3a87
i.galochkin <i.galochkin@argustelecom.ru>
2020-11-27 11:02:26 +0300

    Initial commit

---
+++ Assets/ShipThrust.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipThrust : MonoBehaviour {
+    public float speed = 5.0f;
+    // Перемещает корабль вперед с постоянной скоростью
+    void Update () {
+        var offset = Vector3.forward * Time.deltaTime * speed;
+        this.transform.Translate(offset);
+    }
+}
