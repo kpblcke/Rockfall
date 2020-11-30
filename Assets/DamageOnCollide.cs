9f8e595bd7fa6108ef03976dbe039783851aa770
i.galochkin <i.galochkin@argustelecom.ru>
2020-11-30 13:15:18 +0300

    глава 12 Астероиды и повреждения

---
+++ Assets/DamageOnCollide.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnCollide : MonoBehaviour {
+    // Объем повреждений, наносимых объекту.
+    public int damage = 1;
+    // Объем повреждений, наносимых себе при попадании
+    // в какой-то другой объект.
+    public int damageToSelf = 5;
+    void HitObject(GameObject theObject) {
+        // Нанести повреждение объекту, в который попал данный объект,
+        // если возможно.
+        var theirDamage =
+            theObject.GetComponentInParent<DamageTaking>();
+        if (theirDamage) {
+            theirDamage.TakeDamage(damage);
+        }
+        // Нанести повреждение себе, если возможно
+        var ourDamage =
+            this.GetComponentInParent<DamageTaking>();
+        if (ourDamage) {
+            ourDamage.TakeDamage(damageToSelf);
+        }
+    }
+    // Объект вошел в область действия данного триггера?
+    void OnTriggerEnter(Collider collider) {
+        HitObject(collider.gameObject);
+    }
+    // Другой объект столкнулся с текущим объектом?
+    void OnCollisionEnter(Collision collision) {
+        HitObject(collision.gameObject);
+    }
+}
